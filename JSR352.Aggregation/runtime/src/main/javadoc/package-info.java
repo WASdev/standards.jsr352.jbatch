a5754b74fa17d9e6ac7fa512ce1a3c2efb8237b7
Scott Kurz <skurz@us.ibm.com>
2013-04-23 23:55:50 -0400

    Commit final release, 1.0-b28

---
+++ JSR352.Aggregation/runtime/src/main/javadoc/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * INCOMPLETE:  The com.ibm.jbatch-runtime-all module's Javadoc
+ * is missing.   The reason is that 'com.ibm.jbatch-runtime-all' module
+ * is built by aggregating classes from other Maven modules.  Unfortunately,
+ * we did not complete the task of aggregating the Javadocs as well.  So you
+ * will have to look at the other modules' Javadoc JARs to read the Javadoc 
+ * for the classes in 'com.ibm.jbatch-runtime-all'.
+ * <p>
+ * To sum it up again we have:
+ * <li>com.ibm.jbatch-runtime      =>  Has Javadoc, classes get packaged in runtime-all 
+ * <li>com.ibm.jbatch-model        =>  Has Javadoc, classes get packaged in runtime-all 
+ * <li>com.ibm.jbatch-runtime-all  =>  Classes from the other two, no Javadoc
+ *
+ */
+package javadoc;
