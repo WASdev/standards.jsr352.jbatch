04da25eb87dc07f6839fb2679495e93c8a4081e6
Scott Kurz <skurz@us.ibm.com>
2014-12-19 17:21:19 -0500

    Clean up some copyright headers

--- com.ibm.jbatch.tck/src/main/java/com/ibm/jbatch/tck/artifacts/specialized/ChunkOnErrorCheckpointListener.java
+++ com.ibm.jbatch.tck/src/main/java/com/ibm/jbatch/tck/artifacts/specialized/ChunkOnErrorCheckpointListener.java
@@ -16,30 +16,12 @@
  */
 package com.ibm.jbatch.tck.artifacts.specialized;
 
-/*
- * Copyright 2012 International Business Machines Corp.
- * 
- * See the NOTICE file distributed with this work for additional information
- * regarding copyright ownership. Licensed under the Apache License, 
- * Version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
-*/
-
 import java.util.logging.Logger;
 
 import javax.batch.api.chunk.listener.AbstractChunkListener;
 import javax.batch.runtime.context.JobContext;
 import javax.batch.runtime.context.StepContext;
 import javax.inject.Inject;
-
 
 
 @javax.inject.Named("chunkOnErrorCheckpointListener")
@@ -56,18 +38,18 @@ public class ChunkOnErrorCheckpointListener extends AbstractChunkListener {
 
     @Override
     public void beforeChunk() {
-    	logger.fine("CUSTOMCHKPTLISTENER: beforeCheckpoint");
+        logger.fine("CUSTOMCHKPTLISTENER: beforeCheckpoint");
     }
     
     @Override
     public void afterChunk() {
-    	logger.fine("CUSTOMCHKPTLISTENER: afterCheckpoint");
+        logger.fine("CUSTOMCHKPTLISTENER: afterCheckpoint");
     }
     
     @Override
     public void onError(Exception e) {
-    	logger.fine("CUSTOMCHKPTLISTENER: onError");
-    	
-    	jobCtx.setExitStatus("Chunk onError invoked");
+        logger.fine("CUSTOMCHKPTLISTENER: onError");
+        
+        jobCtx.setExitStatus("Chunk onError invoked");
     }
 }
